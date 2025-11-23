cc6d17f81542721f9ad620a9b009bed3f195ecdd
yeti-rocket <49701955+yeti-rocket@users.noreply.github.com>
2025-11-24 03:08:14 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/yeti/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/yeti/config.h
@@ -37,5 +37,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 4
-#define DYNAMIC_KEYMAP_LAYER_COUNT 9
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
 #define LAYER_STATE_32BIT
